620d199bf403baa7e972dbe4387b3488b9bc50c9
Eluda <111eluda111@gmail.com>
2022-11-05 11:50:07 +0100

    Use long polling for the development environment.

--- environment.d.ts
+++ environment.d.ts
@@ -1,11 +1,11 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        TELEGRAM_TOKEN: string;
-        PORT?: number;
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      TELEGRAM_TOKEN: string;
+      NODE_ENV?: "development" | "production";
+      PORT?: number;
     }
   }
-  
-  export {};
-  
+}
+
+export {};
--- src/bot.ts
+++ src/bot.ts
@@ -11,13 +11,18 @@ bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
 // Handle all other messages
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
 
-// Create the bot's webhooks server
-const app = express();
-app.use(express.json());
-app.use(webhookCallback(bot, "express"));
-
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Bot listening on port ${PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+  // Use Webhooks for the production server
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  // Use Long Polling for development
+  bot.start();
+}
